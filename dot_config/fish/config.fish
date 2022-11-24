ad6530dc91cbc0c49617740d06812699bdd45718
echoriiku <ricardo.ars@pm.me>
2022-11-23 23:15:47 -0300

    lots of updates :)

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -57,7 +57,8 @@ end
     abbr -a upd                       'sudo dnf update --refresh && flatpak update && cargo install-update -a'
 
     alias wezterm='flatpak run org.wezfurlong.wezterm'
+    alias nv='nvim'
 
 #autostart fetch script
 # cutefetch $(printf '\t-k\n-b\n-p' | shuf -n 1) $(shuf -i 1-13 -n 1)
-macchina
+# macchina
